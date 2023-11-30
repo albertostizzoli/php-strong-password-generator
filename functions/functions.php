692c84b3ffbbc218be79396a5195403c89fd5cfb
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-30 16:03:37 +0100

    Complete Milestone 2

---
+++ functions/functions.php
@@ -0,0 +1,13 @@
+<?php
+function generatePassword($length)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($characters) - 1);
+        $password .= $characters[$randomIndex];
+    }
+    return $password;
+}
+?>
--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-function generatePassword($length)
-{
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
-    $password = '';
-
-    for ($i = 0; $i < $length; $i++) {
-        $randomIndex = rand(0, strlen($characters) - 1);
-        $password .= $characters[$randomIndex];
-    }
-    return $password;
-}
+include __DIR__ . '/functions/functions.php';
 ?>
 
 <!DOCTYPE html>
